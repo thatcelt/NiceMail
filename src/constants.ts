a4b9454617a63035c793dee4a197b8e2f3c746c9
thatcelt <fallenincoma@gmail.com>
2026-01-10 12:56:13 +0300

    feat(constants.ts): added new domains

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,11 @@
 export const SITE_URL = 'https://nicemail.cc/ru';
 export const API_URL = 'https://web.nicemail.cc/api/v1';
 
-export const DOMAINS = ['oeralb.com', 'sisood.com', 'disefl.com'];
+export const DOMAINS = [
+  'oeralb.com',
+  'sisood.com',
+  'disefl.com',
+  'suarj.com',
+  'mfxis.com',
+  'anogz.com',
+];
